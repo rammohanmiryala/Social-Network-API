fed9149aba632c39a86d52c6c46499671651d24d
ram <rammohan944@gmail.com>
2022-01-28 01:53:06 +1030

    updated user module

--- models/User.js
+++ models/User.js
@@ -10,41 +10,49 @@ const {
 
 // Schema to create Student model
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    max_length: 50,
-  },
-  email: {
-    type: String,
-    trim: true,
-    lowercase: true,
-    unique: true,
-    required: 'Email address is required',
-    match: [
-      /^([a-z0-9_\.-]+)@([a-z0-9]+)\.([a-z]{2,6})$/,
-      "Please enter a valid email.",
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      max_length: 50,
+    },
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      unique: true,
+      required: 'Email address is required',
+      match: [
+        /^([a-z0-9_\.-]+)@([a-z0-9]+)\.([a-z]{2,6})$/,
+        "Please enter a valid email.",
+      ],
+    },
+    thoughts: [
+      {
+      type: Schema.Types.ObjectId,
+      ref: 'Thought',
+      } 
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      }
+
     ],
   },
-  thoughts: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Thought',
-  }, ],
-  friends: [{
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-  }, ],
+  {
   toJSON: {
     getters: true,
   },
-});
+  id: false,
+  }
+);
 
 
-
-
-const User = model('user', userSchema);
+const User = model('User', userSchema);
 
 module.exports = User;
